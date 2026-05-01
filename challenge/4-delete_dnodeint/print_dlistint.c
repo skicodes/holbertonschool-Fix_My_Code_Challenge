3c55e6a8531c9d91eeb5ffd359e5b96219fa96d2
Sarah K. <skalrashed02@hotmail.com>
2026-05-01 23:18:47 +0300

    Create print_dlistint.c

---
+++ challenge/4-delete_dnodeint/print_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_dlistint - prints all the elements of a doubly linked list
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+    size_t count = 0;
+
+    if (!h)
+        return (0);
+
+    while (h)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        count++;
+    }
+
+    return (count);
+}
